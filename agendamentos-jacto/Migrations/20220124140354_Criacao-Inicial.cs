6cd18dfce1d7224b8a644156db9ff84c6a34f839
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-01-24 11:19:58 -0300

    projeto final

---
+++ agendamentos-jacto/Migrations/20220124140354_Criacao-Inicial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace agendamentos_jacto.Migrations
+{
+    public partial class CriacaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agenda",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Local = table.Column<string>(type: "text", nullable: false),
+                    Cep = table.Column<string>(type: "text", nullable: false),
+                    Endereco = table.Column<string>(type: "text", nullable: false),
+                    Bairro = table.Column<string>(type: "text", nullable: false),
+                    Cidade = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<string>(type: "text", nullable: false),
+                    Detalhes = table.Column<string>(type: "text", nullable: false),
+                    Feito = table.Column<bool>(type: "boolean", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    FinalizacaoEstimada = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    FinalizadoEm = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    User = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agenda", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agenda");
+        }
+    }
+}
