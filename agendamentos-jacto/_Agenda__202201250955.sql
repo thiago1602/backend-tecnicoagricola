67539e39d0499d0f087c54fce556449b412a19d4
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-01-25 08:56:46 -0300

    database

---
+++ agendamentos-jacto/_Agenda__202201250955.sql
@@ -0,0 +1,5 @@
+INSERT INTO public."Agenda" ("Local","Cep","Endereco","Bairro","Cidade","Estado","Detalhes","Feito","CriadoEm","FinalizacaoEstimada","FinalizadoEm","User") VALUES 
+('Fazenda do Estado','17523893','Violetas,29','Centro','Marilia','SP','Foi isso isso isso',true,'2022-01-24 11:15:30.077','2022-01-25 17:20:00.000','2022-01-25 18:26:00.000','thiago@jacto.com')
+,('Fazenda do Joao','17570000','Violetas,29','Centro','Oriente','SP','Foi isso isso isso',true,'2022-01-24 20:46:21.701','2022-01-26 20:45:00.000','2022-01-27 20:45:00.000','thiago@teste.com')
+,('Fazenda do Estado','17523893','Rua Anelda Volta Brazini','Antonio Carlos Nascimento da Silva','Marília','SP','teste teste',true,'2022-01-24 23:06:48.612','2022-01-25 23:10:00.000','2022-01-25 23:12:00.000','thiago@jacto.com')
+;
